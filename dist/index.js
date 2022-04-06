b1610ca1f87369d39ad2ba1f5f34c0f68803d4b6
Irakli <koregm@gmail.com>
2022-04-06 10:33:14 +0400

    Arrays & Tuples

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,15 @@ let x = "Hello";
 x = 3;
 let age;
 age = 4;
+// Arrays
+let ids = [1, 2, 3, 4, 5, 6];
+let arr = [true, 4, "String"];
+// Tuples
+let person = [1, "Text", false];
+// Tuple Array
+let employ;
+employ = [
+    [2, "text"],
+    [1, "John"],
+    [3, "Mike"],
+];
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,18 @@ x = 3;
 
 let age: number;
 age = 4;
+
+// Arrays
+let ids: number[] = [1, 2, 3, 4, 5, 6];
+let arr: any[] = [true, 4, "String"];
+
+// Tuples
+let person: [number, string, boolean] = [1, "Text", false];
+
+// Tuple Array
+let employ: [number, string][];
+employ = [
+  [2, "text"],
+  [1, "John"],
+  [3, "Mike"],
+];
